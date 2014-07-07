0b9616f998df1ab111366f7e3a0a497ba2a6740d
Vania S <vaniaspeedy@gmail.com>
2014-07-06 20:41:58 -0700

    added templates for all services

--- js/functions.js
+++ js/functions.js
@@ -18,6 +18,15 @@
 		
 		// init dropdown
 		$( '#cd-dropdown' ).dropdown();
+		
+		
+		
+		// sliding
+		var ww = $(window).width();
+		$("#container").width(ww * 2);
+		$("#left").width(ww);
+		$("#right").width(ww);
+		
 	
 	});
 	
@@ -39,8 +48,21 @@
 
 /* Creates two eqaul columns. */
 function init2col() {
-    $("#left").width('50%');
-    $("#right").width('50%');
+    var ww = $(window).width();
+    
+        $("#left").animate({
+        width: ww/2
+        
+    }, "easeIn");
+    
+    
+    // start collapsing the left, and slide in the right as we go.
+    $("#right").animate({
+        width: ww/2
+    }, "easeIn");
+    
+    //$("#container").width(ww);
+
 }
 
 
@@ -60,7 +82,7 @@ var API_BASE_URL = 'http://puma/polymer/api';
 
 // Returns status of data1
 function getData1Status() {
-    var data1StatusUrl = API_BASE_URL + '/status/data1'; //.json?
+    var data1StatusUrl = API_BASE_URL + '/status/data1';
     $.ajax({url:data1StatusUrl, type:"GET", dataType:'json', success:function(result){
         //console.log(result);
         var parsed = JSON.parse(result);
@@ -71,13 +93,36 @@ function getData1Status() {
         }
         $("#ajax-detailed-results").html(html);
     }});
+    
+    // perform animations
+    init2col();
+}
+
+
+// Returns status of md0
+function getMd0Status() {
+    var data1StatusUrl = API_BASE_URL + '/status/md0';
+    $.ajax({url:data1StatusUrl, type:"GET", dataType:'json', success:function(result){
+        //console.log(result);
+        var parsed = JSON.parse(result);
+        var html = '';
+        for(var i = 0; i < parsed.length; i++)
+        {
+            html += parsed[i] + '<br>';
+        }
+        $("#ajax-detailed-results").html(html);
+    }});
+    
+    // perform animations
+    init2col();
 }
 
 
 
 
-
-
+function namefuckyou() {
+    // get property value of cd-dropdown to determine what to show.
+}
 
 
 
